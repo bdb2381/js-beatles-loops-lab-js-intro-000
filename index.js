83019c746b19fe322e59973c32b2469dabfa3f15
Bradley Beggs <brad.beggs@gmail.com>
2020-06-12 02:54:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 // add solution here
-var musicians = ["John", "Bach", "Lady Gaga"]
-var instruments = ["guitar", "horn", "Piano"]
+//var musicians = ["John", "Bach", "Lady Gaga"]
+//var instruments = ["guitar", "horn", "Piano"]
 //var empty = []
 
 function theBeatlesPlay(musicians, instruments){
   for (let num = 0; num < musicians.length; num+=1){
       var emptyArray = []
       emptyArray[num] = [`${musicians[num]} plays ${instruments[num]}` ]
-
-
   }
+
 return emptyArray
 
 }
