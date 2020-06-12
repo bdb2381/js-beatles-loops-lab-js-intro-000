18ee91b5472bc8a3781e1516c8fb33ff1e81d1d7
Bradley Beggs <brad.beggs@gmail.com>
2020-06-12 02:55:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var emptyArray = []
 function theBeatlesPlay(musicians, instruments){
   for (let num = 0; num < musicians.length; num+=1){
 
-      emptyArray[num+1] = [`${musicians[num]} plays ${instruments[num]}` ]
+      emptyArray[num] = [`${musicians[num]} plays ${instruments[num]}` ]
   }
 
 return emptyArray
