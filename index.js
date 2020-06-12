fdbcc4bcb1197bea053553b3c8e39675c21b0969
Bradley Beggs <brad.beggs@gmail.com>
2020-06-12 03:44:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function johnLennonFacts(johnFactsArray){
   let counter = 0
   while (counter <= johnFactsArray){
     //JohnFactsArray.push("!!!")
-    let newFactsArray.push(johnFactsArray[num] + "!!!"])
+     newFactsArray.push(johnFactsArray[num] + "!!!"])
     console.log(newFactsArray)
     counter += 1
   }
