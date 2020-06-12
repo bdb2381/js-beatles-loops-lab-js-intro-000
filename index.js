b843c2f1bc067724112c6a70097d922c0c13dada
Bradley Beggs <brad.beggs@gmail.com>
2020-06-12 03:14:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-// add solution here
-//var musicians = ["John", "Bach", "Lady Gaga"]
-//var instruments = ["guitar", "horn", "Piano"]
-//var empty = []
 var emptyArray = []
 
 function theBeatlesPlay(musicians, instruments){
   for (let num = 0; num < 4; num+=1){
 
       emptyArray.push( `${musicians[num]} plays ${instruments[num]}` )
-      console.log(emptyArray)
+      //console.log(emptyArray)    use log to see function in action to debug
   }
 
 return emptyArray
 
+}
+
+
+function johnLennonFacts(johnFactsArray){
+  counter = 0
+  while (counter <= johnFactsArray){
+    [...johnFactsArray, "!!!"]
+    console.log(johnFactsArray
+    counter+=1
+  }
+
+    
+  }
+
+  
 }
