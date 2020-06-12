33a9861ee35d7269120e3f8d36ef841108a0029f
Bradley Beggs <brad.beggs@gmail.com>
2020-06-12 02:34:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // add solution here
+var musicians = ["John", "Bach", "Lady Gaga"]
+var instruments = ["guitar", "horn", "Piano"]
+var empty = []
+
+function theBeatlesPlay(musicians, instruments){
+  for (let num = 0; num < 10; num+=1){
+
+
+    
+  }
+
+  
+}
